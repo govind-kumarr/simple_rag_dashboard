a1ec38dff487d19c2e664a9d4dd128d71318b128
govind-kumarr <gk4051668@gmail.com>
2024-03-06 19:15:25 +0530

    created initialize chatbot fn at index.js

--- chatbot/webpack.config.js
+++ chatbot/webpack.config.js
@@ -1,9 +1,13 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [new HtmlWebpackPlugin()]
 };
