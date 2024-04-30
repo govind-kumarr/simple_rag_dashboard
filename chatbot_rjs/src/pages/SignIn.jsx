d014f1b0e4d545036d4bbdc029d41f5f51f8d9a1
Govind Kumar <gk4051668@gmail.com>
2024-04-30 21:55:44 +0530

    changed dockTalk to doctalk

--- chatbot_rjs/src/pages/SignIn.jsx
+++ chatbot_rjs/src/pages/SignIn.jsx
@@ -51,7 +51,7 @@ const SignIn = () => {
             className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
           >
             <img className="w-8 h-8 mr-2" src="/favicon-32x32.png" alt="logo" />
-            DockTalk
+            DocTalk
           </a>
           <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
